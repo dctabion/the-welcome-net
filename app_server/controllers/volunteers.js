4191312819e5af6d19595bc0c71768e16dc1fea4
Daniel Tabion <dctabion@gmail.com>
2016-07-15 11:24:26 -0500

    added interations for dynamic drop downs / inputs

--- app_server/controllers/volunteers.js
+++ app_server/controllers/volunteers.js
@@ -1,5 +1,33 @@
 module.exports.register = function(req, res) {
-  res.render('register', { title: 'Register a Volunteer' });
+  res.render('register', {
+    title: 'Register a Volunteer',
+    affiliations: {
+      1: "McDonald's",
+      2: "White Castle's",
+      3: "Sam's Salad Bar (uggh)"
+    },
+    hear_abouts: {
+      1: "The grapevine",
+      2: "A little bird",
+      3: "The chinese telephone"
+    },
+    languages: {
+      1: "croatian",
+      2: "serbian",
+      3: "vulcan"
+    },
+    how_oftens: {
+      1: "monthly",
+      2: "twice per month",
+      3: "weekly"
+    },
+    times_of_day: {
+      1: "morning",
+      2: "afternoon",
+      3: "evening",
+      4: "flexible"
+    }
+  });
 };
 
 module.exports.search = function(req, res) {
