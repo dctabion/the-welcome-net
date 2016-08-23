2f5d85d856ca2f16d16a16ed3a07d9f21932f0f5
Daniel Tabion <dctabion@gmail.com>
2016-08-23 08:55:37 -0500

    fixed server PATH for API call

--- app_server/controllers/volunteers.js
+++ app_server/controllers/volunteers.js
@@ -3,7 +3,7 @@ var apiOptions = {
   server: "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
-  apiOptions.server = "https://morning-scrubland-42645.herokuapp.com/";
+  apiOptions.server = "https://morning-scrubland-42645.herokuapp.com";
 }
 
 
