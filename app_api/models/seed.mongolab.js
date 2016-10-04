4bf887846247a60f95dc2de950d0f3773dbe3e70
Daniel Tabion <dctabion@gmail.com>
2016-10-04 16:03:53 -0500

    added mongolab seed file

---
+++ app_api/models/seed.mongolab.js
@@ -0,0 +1,251 @@
+{
+    "_id": {
+        "$oid": "57d549ef106b2a2ef4e557cb"
+    },
+    "affiliations": [
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557a4"
+            },
+            "displayText": "The Welcome Network"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557a5"
+            },
+            "displayText": "Faith Church (Dyer, IN)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557a6"
+            },
+            "displayText": "Faith Church (Hammond, IN)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557a7"
+            },
+            "displayText": "South Side Christian Church (Munster, IN)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557a8"
+            },
+            "displayText": "Mercy Hill Church (Munster, IN)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557a9"
+            },
+            "displayText": "Lake Hills Baptist Church (Schererville, IN)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557aa"
+            },
+            "displayText": "Portage Resale Shop (Portage, IN)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557ab"
+            },
+            "displayText": "Central Church of Christ (Streator, IL)"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557ac"
+            },
+            "displayText": "Bethel Church (Crown Point, IN)"
+        }
+    ],
+    "hearAbouts": [
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557ad"
+            },
+            "displayText": "From a Friend"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557ae"
+            },
+            "displayText": "Facebook"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557af"
+            },
+            "displayText": "Twitter"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b0"
+            },
+            "displayText": "Other"
+        }
+    ],
+    "languages": [
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b1"
+            },
+            "displayText": "English"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b2"
+            },
+            "displayText": "Spanish"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b3"
+            },
+            "displayText": "Arabic"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b4"
+            },
+            "displayText": "Burmese"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b5"
+            },
+            "displayText": "Somali"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b6"
+            },
+            "displayText": "Farsie Nepali"
+        }
+    ],
+    "howOftens": [
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b7"
+            },
+            "displayText": "Monthly"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b8"
+            },
+            "displayText": "Twice per month"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557b9"
+            },
+            "displayText": "Weekly"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557ba"
+            },
+            "displayText": "Contact me for availability"
+        }
+    ],
+    "timesOfDay": [
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557bb"
+            },
+            "displayText": "Weekday mornings"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557bc"
+            },
+            "displayText": "Weekday afternoons"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557bd"
+            },
+            "displayText": "Weekday evenings"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557be"
+            },
+            "displayText": "Weekend mornings"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557bf"
+            },
+            "displayText": "Weekend afternoons"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c0"
+            },
+            "displayText": "Weekend evenings"
+        }
+    ],
+    "opportunityCategories": [
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c1"
+            },
+            "displayText": "English Tutoring"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c2"
+            },
+            "displayText": "Good Neighbor Team"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c3"
+            },
+            "displayText": "Administrative"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c4"
+            },
+            "displayText": "Special Events"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c5"
+            },
+            "displayText": "Legal Clinic Administrative Volunteer"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c6"
+            },
+            "displayText": "Social Media"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c7"
+            },
+            "displayText": "5K Race Volunteer or Participant"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c8"
+            },
+            "displayText": "Church Furniture/Household Goods Donation Coordinator"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557c9"
+            },
+            "displayText": "Refugee Family Home Visits"
+        },
+        {
+            "_id": {
+                "$oid": "57d549ef106b2a2ef4e557ca"
+            },
+            "displayText": "Refugee Family Ride Giver to Medical Screenings"
+        }
+    ]
+}
