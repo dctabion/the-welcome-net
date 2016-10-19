bf4ce794cf8a2296c5e19cd378deee3306ef4936
Daniel Tabion <dctabion@gmail.com>
2016-10-19 17:15:51 -0500

    show/hide added for other language

--- public/javascript/registration_form.js
+++ public/javascript/registration_form.js
@@ -1,3 +1,16 @@
+$('#language_other').change(function() {
+  // if ($('#language_other').checked) {
+  if ($('#language_other').is(':checked')) {
+    console.log('show');
+    $('#other-language-container').show();
+  }
+  else {
+    console.log('hide');
+    // TODO remove the value
+    $('#other-language-container').hide();
+  }
+});
+
 $('select[name=affiliation]').change(function () {
     if ($(this).val() == 'other') {
         console.log('show');
